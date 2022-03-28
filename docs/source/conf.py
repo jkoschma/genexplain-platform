5c9ecd65f546510b44c9380a3986562c80741c8f
jkoschma <43376360+jkoschma@users.noreply.github.com>
2022-03-28 12:00:37 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'recommonmark'
-    'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
