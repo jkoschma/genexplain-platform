1d6f94843ee3fd2e0a0143cb0c43e77bd5549245
jkoschma <43376360+jkoschma@users.noreply.github.com>
2022-03-28 13:30:28 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
- import os
- import sys
- sys.path.insert(0, os.path.abspath('.'))
     
 # for Sphinx-1.4 or newer
 import recommonmark
@@ -183,12 +180,12 @@ epub_exclude_files = ['search.html']
 # At the bottom of conf.py
 def setup(app):
     app.add_config_value('recommonmark_config', {
-            #'url_resolver': lambda url: github_doc_root + url,
-            #'auto_toc_tree_section': 'Contents',
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
             'enable_math': False,
             'enable_inline_math': False,
             'enable_eval_rst': True,
-            #'enable_auto_doc_ref': True,
+            'enable_auto_doc_ref': True,
     }, True)
     app.add_transform(AutoStructify)
 
